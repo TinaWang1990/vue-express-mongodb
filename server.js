ac0934adc73653c48e88e11fc7a582409a78f8c4
Tina Wang <tina.wang@quantropi.com>
2018-12-14 16:17:01 -0500

    all

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors= require('cors');
+
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const posts = require('./server/routes/api/posts');
 
 app.use('/api/posts', posts);
+
+//handle production
+if (process.env.NODE_ENV === 'production'){
+  //static folder
+  app.use(express.static(__dirname+'/public'));
+
+  //handle single page app
+  app.get(/.*/, (req, res)=>res.sendFile(__dirname+'/public/index.html'));
+}
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
